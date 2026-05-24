27ff26b712aa3a399b1e888d16bb55924ea0b116
Alexander Olsson <alex.olsson.93@gmail.com>
2026-05-24 11:18:17 +0200

    Inital hello world print.

---
+++ conanfile.py
@@ -0,0 +1,42 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+
+class VulkanTriangle(ConanFile):
+    name        = "opengl-triangle"
+    settings    = "os", "compiler", "build_type", "arch"
+    version     = "0.0.1"
+    description = "This is a basic application for rendering a triangle with OpenGL in C++ using Conan and CMake."
+    author      = "Alexander Olsson (alex.olsson.93@gmail.com)"
+    url         = "https://github.com/Znojse/vulkan-triangle"
+
+    def requirements(self):
+        pass
+        # if self.settings.compiler == "msvc" or self.settings.os == "Linux":
+        #     self.requires("glfw/3.4")  # Package is built with MSVC in Windows and does not work with [Windows, mingw] setup.
+
+        # self.requires("glm/1.0.1")
+
+        # Requires Vulkan SDK installed build environment.
+        # self.requires("vulkan-loader/1.4.313.0")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.29]")
+        self.test_requires("catch2/[>=3.9.1]")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = 'ConanPresets.json'
+        tc.generate()
+
+    def layout(self):
+        cmake_layout(self)
+
+    def validate(self):
+        pass
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+
+auto main() -> int {
+    std::cout << "Hello OpenGL Triangle!\n";
+
+    // try {
+    //     app.Run();
+    // } catch (const std::exception& e) {
+    //     std::cerr << e.what() << "\n";
+    //     return EXIT_FAILURE;
+    // }
+
+    return EXIT_SUCCESS;
+}
